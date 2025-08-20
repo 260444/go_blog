0e4b669ff468c1d7151ef58918d121bc1c4d0a15
下了雪的哈尔滨 <2603485744@qq.com>
2025-08-20 14:24:34 +0800

    feat:后端框架及核心文件

---
+++ create_dir.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+mkdir server\api
+mkdir server\assets
+mkdir server\config
+mkdir server\core
+mkdir server\flag
+mkdir server\global
+mkdir server\initialize
+mkdir server\log
+mkdir server\middleware
+mkdir server\model\appTypes
+mkdir server\model\database
+mkdir server\model\elasticsearch
+mkdir server\model\other
+mkdir server\model\request
+mkdir server\model\response
+mkdir server\router
+mkdir server\service
+mkdir server\task
+mkdir server\uploads
+mkdir server\utils\hotSearch
+mkdir server\utils\upload
+
+echo 目录创建完成
